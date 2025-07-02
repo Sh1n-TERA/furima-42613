f5a6d4e2a81552b03431907b3bc5820679d6f81a
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-02 11:44:26 +0900

    emailに関するテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,54 +26,63 @@ RSpec.describe User, type: :model do
       end
 
       it 'emailが重複していると登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
 
       it 'emailに@が含まれていないと登録できない'
-    end
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
 
-    it 'passwordが空では登録できない' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
 
-    it 'passwordが6文字未満では登録できない' do
-    end
+      it 'passwordが6文字未満では登録できない' do
+      end
 
-    it 'passwordが英字のみでは登録できない' do
-    end
+      it 'passwordが英字のみでは登録できない' do
+      end
 
-    it ' passwordが数字のみでは登録できない' do
-    end
+      it ' passwordが数字のみでは登録できない' do
+      end
 
-    it 'passwordとpassword_confirmationが一致していないと登録できない' do
-    end
+      it 'passwordとpassword_confirmationが一致していないと登録できない' do
+      end
 
-    it 'last_name が空だと登録できない' do
-    end
+      it 'last_name が空だと登録できない' do
+      end
 
-    it 'first_name が空だと登録できない' do
-    end
+      it 'first_name が空だと登録できない' do
+      end
 
-    it 'last_name が全角でないと登録できない' do
-    end
+      it 'last_name が全角でないと登録できない' do
+      end
 
-    it 'first_name が全角でないと登録できない' do
-    end
+      it 'first_name が全角でないと登録できない' do
+      end
 
-    it 'last_name_kana が空だと登録できない' do
-    end
+      it 'last_name_kana が空だと登録できない' do
+      end
 
-    it 'first_name_kana が空だと登録できない' do
-    end
+      it 'first_name_kana が空だと登録できない' do
+      end
 
-    it 'last_name_kana がカタカナでないと登録できない' do
-    end
+      it 'last_name_kana がカタカナでないと登録できない' do
+      end
 
-    it 'first_name_kana がカタカナでないと登録できない' do
-    end
+      it 'first_name_kana がカタカナでないと登録できない' do
+      end
 
-    it 'birthday が空だと登録できない' do
+      it 'birthday が空だと登録できない' do
+      end
     end
   end
 end
