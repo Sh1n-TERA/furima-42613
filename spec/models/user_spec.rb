2a889208adfd5735305e43e7371705a5e38e0fa2
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-02 11:33:11 +0900

    テストコードの土台（example）作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,11 +25,55 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
 
-      it 'passwordが空では登録できない' do
-        @user.password = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
+      it 'emailが重複していると登録できない' do
       end
+
+      it 'emailに@が含まれていないと登録できない'
+    end
+
+    it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'passwordが6文字未満では登録できない' do
+    end
+
+    it 'passwordが英字のみでは登録できない' do
+    end
+
+    it ' passwordが数字のみでは登録できない' do
+    end
+
+    it 'passwordとpassword_confirmationが一致していないと登録できない' do
+    end
+
+    it 'last_name が空だと登録できない' do
+    end
+
+    it 'first_name が空だと登録できない' do
+    end
+
+    it 'last_name が全角でないと登録できない' do
+    end
+
+    it 'first_name が全角でないと登録できない' do
+    end
+
+    it 'last_name_kana が空だと登録できない' do
+    end
+
+    it 'first_name_kana が空だと登録できない' do
+    end
+
+    it 'last_name_kana がカタカナでないと登録できない' do
+    end
+
+    it 'first_name_kana がカタカナでないと登録できない' do
+    end
+
+    it 'birthday が空だと登録できない' do
     end
   end
 end
