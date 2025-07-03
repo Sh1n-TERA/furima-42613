583d0e57ab5b68be39bcdfede7cfa55402e572ab
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-04 00:14:37 +0900

    itemsモデルにバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :shipping_day_id
+    end
+
+    with_options numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 } do
+      validates :price
+    end
+  end
+
   belongs_to :user
   has_one_attached :image
 end
