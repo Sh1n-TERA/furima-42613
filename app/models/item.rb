5864a09035ea88786cad4bfbf2a6ce78559a00c8
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-04 15:13:50 +0900

    shipping_day用のActiveHashモデル作成、itemモデルに関連付

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :delivery_fee
   belongs_to :prefecture
+  belongs_to :shipping_day
 
   with_options presence: true do
     validates :image
