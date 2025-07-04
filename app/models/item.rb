af2d0af4e21d1b57737ea5872ffa553eaea83f13
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-04 13:01:31 +0900

    category用のActiveHashモデル作成、itemモデルに関連付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   with_options presence: true do
     validates :image
     validates :name
