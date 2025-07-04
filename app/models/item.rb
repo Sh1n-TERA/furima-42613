0442c39b9d2d0c990f30e2d15d77a0a8008f14ee
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-04 15:06:37 +0900

    prefecture用のActiveHashモデル作成、itemモデルに関連付

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_fee
+  belongs_to :prefecture
 
   with_options presence: true do
     validates :image
