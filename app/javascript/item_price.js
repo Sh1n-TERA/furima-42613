76a9bd5142b07a4d58e729b52015c8044982b9e7
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-08 14:53:20 +0900

    出品に失敗javascriptが動作するよう修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,13 @@
-window.addEventListener('turbo:load', () => {
-  const priceInput = document.getElementById("item-price");
+document.addEventListener("turbo:load", () => {
+  setupPriceListener();
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+  setupPriceListener();
+});
+
+function setupPriceListener() {
+  const priceInput = document.getElementById("item-price");
   if (!priceInput) return;
 
   const addTaxDom = document.getElementById("add-tax-price");
@@ -9,7 +16,6 @@ window.addEventListener('turbo:load', () => {
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
 
-    // 半角数字以外は弾く（optional: バリデーション強化したい場合）
     if (inputValue.match(/^\d+$/)) {
       const tax = Math.floor(inputValue * 0.1);
       const profit = Math.floor(inputValue - tax);
@@ -20,4 +26,4 @@ window.addEventListener('turbo:load', () => {
       profitDom.innerHTML = "";
     }
   });
-});
+}
