bc24a288020aa6dbfdead67bcc6bf3afea1f5c9c
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-14 17:53:49 +0900

    購入ページのアクセス制限処理を実装（未ログイン・出品者・購入済み）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,27 +1,38 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :redirect_when_invalid_user_or_order
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_address_params)
 
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
-      logger.debug "❌ バリデーションエラー: #{@order_address.errors.full_messages}"
       render :index
     end
   end
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def order_address_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
+  end
+
+  def redirect_when_invalid_user_or_order
+    return unless current_user == @item.user || @item.order.present?
+
+    redirect_to root_path
   end
 end
