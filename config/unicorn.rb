9c69907160f28f84ec865a54dcd2260ef91050b2
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-21 21:50:23 +0900

    unicorn.rb を本番環境の構成に合わせて修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,42 +1,37 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+# config/unicorn.rb
 
-#アプリケーションサーバの性能を決定する
+# アプリケーションのルートパス（sharedとcurrentの親）
+app_path = File.expand_path('../../../', __FILE__)
+
+# プロセス数
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+# アプリの実行ディレクトリ
+working_directory "#{app_path}/current"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+# PIDファイル
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
-listen "/var/www/furima-42613/shared/tmp/sockets/unicorn.sock"
+# ソケット通信の設定（NginxとUnicornをつなぐ）
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+# ログ出力
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
-
-#Railsアプリケーションの応答を待つ上限時間を設定
+# 応答待ち時間
 timeout 60
-
-#以下は応用的な設定なので説明は割愛
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
 check_client_connection false
-
 run_once = true
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
-
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
   if run_once
-    run_once = false # prevent from firing again
+    run_once = false
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
